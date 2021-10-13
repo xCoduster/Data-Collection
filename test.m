23bf6a7bdbfc3094d61e23c3a94d40f277c5612c
xCoduster <xcoduster@outlook.com>
2021-10-13 17:29:56 +0200

    Program almost done

--- test.m
+++ test.m
@@ -1,13 +1,17 @@
+rate = 10;
+counter = 0;
+captureTime = 5;
+
 daqlist("ni")
 dq = daq("ni");
-dq.Rate = 1;
-addinput(dq, "Dev2", "ai0", "Voltage");
+dq.Rate = rate;
+addinput(dq, "Dev1", "ai0", "Voltage");
 
 data = timetable();
 
 for i = 0:1
     dataTmp = read(dq, seconds(5));
-    dataTmp.Time = dataTmp.Time;
+    dataTmp.Time = dataTmp.Time + seconds(0);
     data = [data; dataTmp]
     dataTmp = 0;
 end
